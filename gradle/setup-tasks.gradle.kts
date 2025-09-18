dd1542aa23e4141997c7de3b88344ebce4116624
Chris (손영철) <chris8282@nol-universe.com>
2025-09-18 09:10:34 +0900

    ops: refactored task code

--- gradle/setup-tasks.gradle.kts
+++ gradle/setup-tasks.gradle.kts
@@ -9,32 +9,34 @@ tasks.register("setupTestInfra") {
 fun createReposDirectory() {
     val reposDir = file("repos")
 
-    if (reposDir.exists().not()) {
-        reposDir.mkdirs()
-        println("Created 'repos' directory")
-    } else {
+    if (reposDir.exists()) {
         println("'repos' directory already exists")
+        return
     }
+
+    reposDir.mkdirs()
+    println("Created 'repos' directory")
 }
 
 fun cloneKioskRepository() {
     val kioskRepoDir = file("repos/atdd-camping-kiosk")
 
-    if (!kioskRepoDir.exists()) {
-        exec {
-            commandLine(
-                "git", "clone",
-                "--depth", "1",
-                "--single-branch",
-                "--branch", "main",
-                "https://github.com/next-step/atdd-camping-kiosk.git",
-                "repos/atdd-camping-kiosk",
-            )
-        }
-        println("Successfully cloned atdd-camping-kiosk repository")
-    } else {
+    if (kioskRepoDir.exists()) {
         println("atdd-camping-kiosk repository already exists")
+        return
     }
+
+    exec {
+        commandLine(
+            "git", "clone",
+            "--depth", "1",
+            "--single-branch",
+            "--branch", "main",
+            "https://github.com/next-step/atdd-camping-kiosk.git",
+            "repos/atdd-camping-kiosk",
+        )
+    }
+    println("Successfully cloned atdd-camping-kiosk repository")
 }
 
 fun dockerComposeUp() {
