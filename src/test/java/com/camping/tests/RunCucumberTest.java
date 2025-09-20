7e46956db8c2090e5a77cee52fca5d5e2e738e5b
Chris (손영철) <chris8282@nol-universe.com>
2025-09-20 21:37:16 +0900

    test: 프로젝트 내 코드만 scan 하도록 테스트 옵션 추가

--- src/test/java/com/camping/tests/RunCucumberTest.java
+++ src/test/java/com/camping/tests/RunCucumberTest.java
@@ -1,10 +1,14 @@
 package com.camping.tests;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.camping.tests")
 public class RunCucumberTest {
 }
 
