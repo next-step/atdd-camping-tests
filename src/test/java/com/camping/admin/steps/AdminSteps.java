6f0b29c4f3990e89de473eaac999ff84d734a0f0
MoonJeWoong <munjin0201@naver.com>
2025-09-21 23:31:58 +0900

    test: 상품 목록 조회 E2E 테스트 작성

--- src/test/java/com/camping/admin/steps/AdminSteps.java
+++ src/test/java/com/camping/admin/steps/AdminSteps.java
@@ -3,8 +3,11 @@ package com.camping.admin.steps;
 import com.camping.common.support.AdminApiHelper;
 import com.camping.common.support.CommonContext;
 import io.cucumber.core.options.CurlOption.HttpMethod;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
 public class AdminSteps {
 
@@ -17,5 +20,18 @@ public class AdminSteps {
         CommonContext.lastResponse = AdminApiHelper.request(HttpMethod.GET, "/", null)
                 .then().log().all()
                 .extract().response();
+    }
+
+    @When("상품 목록 조회를 요청한다")
+    public void 상품목록조회를요청한다() {
+        CommonContext.lastResponse = AdminApiHelper.request(HttpMethod.GET, "admin/products", null)
+                .then().log().all()
+                .extract().response();
+    }
+
+    @Then("현재 상품 정보들을 받는다")
+    public void 현재상품정보들을받는다() {
+        CommonContext.lastResponse.then()
+                .body("size()", greaterThanOrEqualTo(1));
     }
 }
--- src/test/resources/features/admin.feature
+++ src/test/resources/features/admin.feature
@@ -4,4 +4,6 @@ Feature: admin 서버 컨테이너 스모크 테스트
     When admin 컨테이너에 요청을 보낸다
     Then 성공 응답을 받는다
 
-
+  Scenario: 관리자가 상품 목록을 조회할 수 있다
+    When 상품 목록 조회를 요청한다
+    Then 현재 상품 정보들을 받는다
