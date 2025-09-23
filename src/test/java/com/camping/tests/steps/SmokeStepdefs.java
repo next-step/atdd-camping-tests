34a7e4cce914c245bd541361d927daa85fac3d4a
StoneHee99 <kiit0901@gmail.com>
2025-09-23 21:57:16 +0900

    feat: 신규 서비스 smoke 테스트 작성

--- src/test/java/com/camping/tests/steps/SmokeStepdefs.java
+++ src/test/java/com/camping/tests/steps/SmokeStepdefs.java
@@ -2,17 +2,18 @@ package com.camping.tests.steps;
 
 import static io.restassured.RestAssured.given;
 
+import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 
-public class SampleSteps {
+public class SmokeStepdefs {
 
     private Response response;
 
     @When("{string}에 요청을 보낸다")
-    public void 요청을보낸다(String url) {
+    public void 에요청을보낸다(String url) {
         response = given()
                 .when().get(url)
                 .then().log().all()
@@ -24,5 +25,3 @@ public class SampleSteps {
         response.then().statusCode(HttpStatus.SC_OK);
     }
 }
-
-
---
+++ src/test/resources/features/smoke.feature
@@ -0,0 +1,12 @@
+Feature: 모든 인프라가 잘 동작하는지 검증
+    Scenario: 키오스크 서비스 동작 확인
+        When "http://localhost:8080"에 요청을 보낸다
+        Then 성공 응답을 받는다
+
+    Scenario: 관리자 서비스 동작 확인
+        When "http://localhost:8081"에 요청을 보낸다
+        Then 성공 응답을 받는다
+
+    Scenario: 예약 서비스 동작 확인
+        When "http://localhost:8082"에 요청을 보낸다
+        Then 성공 응답을 받는다
