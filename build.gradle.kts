546f8f5697af3573c29cefc288e23a4e0788ec72
donggi-lee-bit <devdonggilee@gmail.com>
2025-09-24 09:03:52 +0900

    build: AssertJ 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     testImplementation("org.junit.platform:junit-platform-suite:1.10.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("org.assertj:assertj-core:3.25.1")
     testRuntimeOnly("org.junit.platform:junit-platform-suite-engine:1.10.0")
 
     // JDBC driver for test hooks
