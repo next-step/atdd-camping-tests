d1f57e8f6d035a48f945c40a4e0e4e97d5496c54
dahee.cha <daheecha0612@gmail.com>
2025-09-26 21:17:19 +0900

    refactor: 불필요 로직 제거

--- src/test/java/com/camping/tests/helper/RequestSender.java
+++ src/test/java/com/camping/tests/helper/RequestSender.java
@@ -1,7 +1,6 @@
 package com.camping.tests.helper;
 
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 
 import java.util.Map;
 
@@ -39,39 +38,10 @@ public class RequestSender {
     }
 
     public static Response getWithAuth(String baseUrl, String endpoint, String authToken) {
-        RequestSpecification request = given()
+        return given()
                 .baseUri(baseUrl)
-                .log().all(); // 요청 정보 로깅
-
-        if (authToken != null && !authToken.isEmpty()) {
-            // JWT 토큰인 경우 Authorization 헤더에 Bearer 토큰으로 추가
-            if (authToken.startsWith("eyJ")) {
-                request = request.header("Authorization", "Bearer " + authToken);
-            } else {
-                // 쿠키인 경우 Cookie 헤더에 추가
-                request = request.cookie("JSESSIONID", authToken);
-            }
-        }
-
-        return request
-                .when()
-                .get(endpoint)
-                .then()
-                .log().all() // 모든 응답 로깅 (성공/실패 모두)
-                .extract()
-                .response();
-    }
-
-    public static Response getWithCookies(String baseUrl, String endpoint, Map<String, String> cookies) {
-        RequestSpecification request = given()
-                .baseUri(baseUrl)
-                .log().all(); // 요청 정보 로깅
-
-        if (cookies != null && !cookies.isEmpty()) {
-            request = request.cookies(cookies);
-        }
-
-        return request
+                .log().all() // 요청 정보 로깅
+                .header("Authorization", "Bearer " + authToken)
                 .when()
                 .get(endpoint)
                 .then()
