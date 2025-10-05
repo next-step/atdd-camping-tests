cf766e275ae76d6ca6354033592e81baf568f5cb
MoonJeWoong <munjin0201@naver.com>
2025-10-05 17:00:31 +0900

    test: payment wiremock 서비스 smoke 테스트 추가

--- src/test/java/com/camping/common/support/CommonContext.java
+++ src/test/java/com/camping/common/support/CommonContext.java
@@ -17,6 +17,10 @@ public final class CommonContext {
     public static final String RESERVATION_DEFAULT_URL = "http://localhost:18082";
     public static String RESERVATION_BASE_URL;
 
+    public static final String PAYMENTS_MOCK_BASE_URL_PROPERTY_KEY = "PAYMENTS_MOCK_BASE_URL";
+    public static final String PAYMENTS_MOCK_DEFAULT_URL = "http://localhost:18083";
+    public static String PAYMENTS_MOCK_BASE_URL;
+
     public static String adminToken;
     public static RequestSpecification requestSpec;
     public static Response lastResponse;
@@ -25,6 +29,7 @@ public final class CommonContext {
         KIOSK_BASE_URL = System.getProperty(KIOSK_BASE_URL_PROPERTY_KEY, System.getenv().getOrDefault(KIOSK_BASE_URL_PROPERTY_KEY, KIOSK_DEFAULT_URL));
         ADMIN_BASE_URL = System.getProperty(ADMIN_BASE_URL_PROPERTY_KEY, System.getenv().getOrDefault(ADMIN_BASE_URL_PROPERTY_KEY, ADMIN_DEFAULT_URL));
         RESERVATION_BASE_URL = System.getProperty(RESERVATION_BASE_URL_PROPERTY_KEY, System.getenv().getOrDefault(RESERVATION_BASE_URL_PROPERTY_KEY, RESERVATION_DEFAULT_URL));
+        PAYMENTS_MOCK_BASE_URL = System.getProperty(PAYMENTS_MOCK_BASE_URL_PROPERTY_KEY, System.getenv().getOrDefault(PAYMENTS_MOCK_BASE_URL_PROPERTY_KEY, PAYMENTS_MOCK_DEFAULT_URL));
     }
 
 
