ae8cca5c77f9f3eda9847d0a9af74f32210c5c97
Dongyeon Min <dongyeon.min@woowahan.com>
2025-10-08 15:48:36 +0900

    피드백 반영 - payment repo 제거

--- custom-tasks.gradle.kts
+++ custom-tasks.gradle.kts
@@ -188,10 +188,5 @@ object DockerConfig {
             url = "https://github.com/mdy0501/atdd-camping-reservation.git",
             branch = "mdy0501-test"
         ),
-        Repo(
-            name = "atdd-camping-payments",
-            url = "https://github.com/mdy0501/atdd-camping-payments.git",
-            branch = "mdy0501-test"
-        ),
     )
 }
