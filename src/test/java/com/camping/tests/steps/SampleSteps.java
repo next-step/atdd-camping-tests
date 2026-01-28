1539b6d79aa32f2006b7713d4fd2a0fbd60bad80
yhh1056 <58363663+yhh1056@users.noreply.github.com>
2026-01-28 13:19:19 +0900

    키오스크 컨테이너 띄우기 & 테스트 환경 세팅 (#36)

--- src/test/java/com/camping/tests/steps/SampleSteps.java
+++ src/test/java/com/camping/tests/steps/SampleSteps.java
@@ -2,17 +2,30 @@ package com.camping.tests.steps;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 
+import static com.camping.tests.config.ExternalAPIConfig.키오스크_시스템_호스트;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SuppressWarnings("NonAsciiCharacters")
 public class SampleSteps {
+    private ExtractableResponse<Response> response;
 
-    @When("{string}에 요청을 보낸다")
-    public void 요청을보낸다(String url) {
-        System.out.println(url + "에 요청을 보냈다");
+    @When("키오스크에 요청을 보낸다")
+    public void 요청을보낸다() {
+        response = RestAssured.given()
+                .baseUri(키오스크_시스템_호스트())
+                .when()
+                .get("/")
+                .then()
+                .extract();
     }
 
     @Then("성공 응답을 받는다")
     public void 성공응답을받는다() {
-        System.out.println("성공 응답을 받았다");
+        assertThat(response.statusCode()).isEqualTo(200);
     }
 }
 
