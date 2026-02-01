df4adc95cde650b6c625047658f6f10363114006
sunga <eert2260@naver.com>
2026-02-01 19:01:01 +0900

    헬스 체크 시나리오 실제 요청 보내도록 수정

--- src/test/java/com/camping/tests/steps/SampleSteps.java
+++ src/test/java/com/camping/tests/steps/SampleSteps.java
@@ -2,17 +2,23 @@ package com.camping.tests.steps;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SampleSteps {
 
+    private Response response;
+
     @When("{string}에 요청을 보낸다")
     public void 요청을보낸다(String url) {
-        System.out.println(url + "에 요청을 보냈다");
+        response = RestAssured.get(url);
     }
 
     @Then("성공 응답을 받는다")
     public void 성공응답을받는다() {
-        System.out.println("성공 응답을 받았다");
+        assertEquals(200, response.statusCode());
     }
 }
 
