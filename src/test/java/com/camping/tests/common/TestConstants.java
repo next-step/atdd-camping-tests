fa5c7a9f2c34c89a7f077a72bfaa9e9c7d277f74
이진우 <leejinwoo1126@gmail.com>
2026-02-03 22:32:03 +0900

    TestContstants 클래스 생성

---
+++ src/test/java/com/camping/tests/common/TestConstants.java
@@ -0,0 +1,8 @@
+package com.camping.tests.common;
+
+public final class TestConstants {
+    private TestConstants() {}
+
+    public static final String DEFAULT_ORDER_ID = "dummy-order-id";
+    public static final String DEFAULT_PAYMENT_KEY = "dummy-payment-key";
+}
