f4f6b3971040b8b51c686bead34e896e3e977c23
이진우 <leejinwoo1126@gmail.com>
2026-02-03 22:52:35 +0900

    refactor: kiosk, payment endpoint url를 상수 선언

--- src/test/java/com/camping/tests/common/TestConstants.java
+++ src/test/java/com/camping/tests/common/TestConstants.java
@@ -5,4 +5,17 @@ public final class TestConstants {
 
     public static final String DEFAULT_ORDER_ID = "dummy-order-id";
     public static final String DEFAULT_PAYMENT_KEY = "dummy-payment-key";
+
+    public static final class Kiosk {
+        private Kiosk() {}
+        public static final String PRODUCTS_ENDPOINT = "/api/products";
+        public static final String PAYMENTS_ENDPOINT = "/api/payments";
+        public static final String PAYMENTS_CONFIRM_ENDPOINT = "/api/payments/confirm";
+    }
+
+    public static final class Payment {
+        private Payment() {}
+        public static final String PAYMENTS_ENDPOINT = "/v1/payments";
+        public static final String PAYMENTS_CONFIRM_ENDPOINT = "/v1/payments/confirm";
+    }
 }
